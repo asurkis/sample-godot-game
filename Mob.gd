114a4b5ed25b39f520563fd9761bf7fa04299eeb
Anton Surkis <antonsurkis@gmail.com>
2022-01-07 20:45:33 +0300

    Add enemy

---
+++ Mob.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+
+func _ready():
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
