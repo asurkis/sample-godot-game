b8571d3ac3cb90376e4cf438fd0c4fbf206dc2f3
Anton Surkis <antonsurkis@gmail.com>
2022-01-07 22:18:09 +0300

    Change controls

--- Player.gd
+++ Player.gd
@@ -2,29 +2,38 @@ extends Area2D
 
 signal hit
 
+export var acceleration = 2000
 export var speed = 400
 var screen_size
+var target = Vector2()
+var velocity = Vector2()
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	disable()
 
+func _input(event):
+	if event is InputEventScreenDrag:
+		target = event.position
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _process(delta):
-	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	var acc = Vector2()
+	if position.distance_to(target) > 10:
+		acc = target - position
+	if acc.length() > 0:
+		acc = acc.normalized() * acceleration
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-	position += velocity * delta
+	# velocity = speed => resistance = acc
+	# resistance = v * k
+	# speed * k = acceleration
+	var resistance = velocity * (acceleration / speed)
+	var old_velocity = velocity
+	velocity += (acc - resistance) * delta
+	position += (velocity + old_velocity) * (delta / 2)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
